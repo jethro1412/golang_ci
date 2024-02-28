0985887aa71f3b7b001343cf6b3548a10fe83a1a
jethro1412 <kurniawanb182@gmail.com>
2024-02-28 18:19:28 +0700

    add swagger documentation

---
+++ docs/docs.go
@@ -0,0 +1,24 @@
+// Package of our golang swagger api for testing
+//
+// Documentation of our swagger API.
+//
+//     Schemes: http
+//     BasePath: /
+//     Version: 1.0.0
+//     Host: Localhost
+//
+//     Consumes:
+//     - application/json
+//
+//     Produces:
+//     - application/json
+//
+//     Security:
+//     - basic
+//
+//    SecurityDefinitions:
+//    basic:
+//      type: basic
+//
+// swagger:meta
+package docs
